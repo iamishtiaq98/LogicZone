260a2ae5483f5f9cb19429bd2cb6396419a1e1a4
Ishtiaq AMjad <muhammadishtiaqamjad@gmail.com>
2023-09-05 13:57:01 +0500

    letest-final

--- pages/_document.js
+++ pages/_document.js
@@ -1,42 +1,14 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheets } from '@mui/styles';
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    try {
-      const sheets = new ServerStyleSheets();
-      const originalRenderPage = ctx.renderPage;
 
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-
-      // Return the expected object structure with 'html' prop
-      return {
-        ...initialProps,
-        html: initialProps.html, // You may not need to modify this property
-        // Add other properties as needed
-      };
-    } catch (error) {
-      console.error('Error in getInitialProps:', error);
-      return {}; // Return an empty object or handle the error appropriately
-    }
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body style={{ backgroundColor: '#fff', fontFamily: '"Open Sans", sans-serif' }}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
-export default MyDocument;
